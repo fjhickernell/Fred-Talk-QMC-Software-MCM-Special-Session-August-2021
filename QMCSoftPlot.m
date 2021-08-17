78bb6042342e29ae15b15e568b76394778a60846
Fred Hickernell <hickernell@iit.edu>
2021-08-16 22:16:28 -0500

    Fred Home Computer Update

--- QMCSoftPlot.m
+++ QMCSoftPlot.m
@@ -5,14 +5,14 @@ gail.InitializeDisplay
 
 % Software, User Base, Scope
 QMCSoftE = {
-    "SciPy/PyTorch", 1, 0.3
+    "SciPy/PyTorch", 1, 0.55
     "QMCPy", 0.3, 0.8
-    "cuRAND",0.9, 0.2
+    "cuRAND",0.9, 0.3
     "MATLAB",0.97, 0.25
     "qrng",0.6,0.2
     "MPS/QMC4PDE",0.35,0.5
     "LatNet/SSJ",0.55,0.8
-    "UQLab",0.5,0.1
+    "UQLab",0.6,0.1
 };
 SoftNames = QMCSoftE(:,1);
 SoftUser = cell2mat(QMCSoftE(:,2));
@@ -27,8 +27,9 @@ labelpoints(SoftUser,SoftScope,SoftNames,"E",0.2,0,'FontSize',16)
 hold on
 QMCSoftW = {    
     "BRODA",0.85,0.25
-    "Tensorflow", 0.95, 0.3
-    "GAIL",0.3 0.75
+    "Tensorflow", 0.95, 0.55
+    "GAIL",0.3 0.7
+    "OpenTURNS",0.55,0.1
 };
 SoftNames = QMCSoftW(:,1);
 SoftUser = cell2mat(QMCSoftW(:,2));
@@ -36,7 +37,10 @@ SoftScope = cell2mat(QMCSoftW(:,3));
 plot(SoftUser,SoftScope,'.','markerSize',50,'color',MATLABBlue)
 labelpoints(SoftUser,SoftScope,SoftNames,"W",0.2,0,'FontSize',16)
 
-quiver([0.85;1],[0.8;0.35],[0.3;0.1],[0.1;0.5],...
-    'LineWidth',4,'color',MATLABOrange,'AutoScale','off')
+q=quiver(0.9,0.65,0.25,0.25,...
+    'LineWidth',6,'color',MATLABOrange,'AutoScale','off', ...
+    'maxheadsize',1.5)
+%quiver([0.85;1],[0.8;0.65],[0.3;0.2],[0.2;0.35],...
+%    'LineWidth',4,'color',MATLABOrange,'AutoScale','off')
 
 print('-depsc','QMCSoftwarePlot.eps')
